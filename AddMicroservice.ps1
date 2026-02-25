f12b10cdf81dad8807b04183049562b76a310118
MikkelRLarsen <Thenuker95@gmail.com>
2026-02-25 18:45:46 +0100

    Added Template script

---
+++ AddMicroservice.ps1
@@ -0,0 +1,26 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$ProjectName
+)
+
+$templateShortName = "clean-temp"
+
+$installed = dotnet new list | Select-String $templateShortName
+if (-not $installed) {
+	dotnet new install /TemplateFolder
+}
+
+dotnet new clean-temp -n $ProjectName -o src/$ProjectName
+
+$projects = @(
+	"Api",
+	"Domain",
+	"Infrastructure",
+	"Facade",
+	"UnitTest",
+	"UseCase"
+	)
+
+foreach ($project in $projects) {
+	dotnet sln add src/$ProjectName/$ProjectName.$project/$ProjectName.$project.csproj
+}
